c1c0d4a9779c6f5ddf82a7a9d7755b25ea0cfa2c
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2023-12-07 13:16:11 +0530

    TEST

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<title>Home</title>
+</head>
+<body>
+	<main>
+		<header>
+			<div style="    background-color: #022d5d;" class="padding-tb">
+				<div class="grid-3">
+					<div class="text-center white">
+						<h2> Baba Programmer</h2>
+					</div>
+					<div style="    margin: 4px 0px;">
+						<div class="grid-4 container-90">
+						<div class="display-flex white">
+							<i class="material-icons  fs-20">call</i>
+							<p> +91-83 19 63 60 62 </p>
+						</div>
+						<div class="display-flex white">
+							<i class="material-icons fs-20">call</i>
+							<p> +91-96 91 12 42 51 </p>
+						</div>
+						<div class="display-flex white">
+							<i class="material-icons fs-20">call</i>
+							<p>  +91-77 52 49 40 36 </p>
+						</div>
+						<div class="display-flex white" >
+							<i class="material-icons fs-20">email</i>
+							<p>  babaprogramer021@gmail.com</p>
+						</div>
+					</div>
+					</div>
+					
+				</div>
+			</div>
+		</header>
+
+		<nav>
+			<div style="box-shadow:0 -1px 0px 0 rgba(0, 0, 0, 0.2), 0 5px 17px 0 rgba(0, 0, 0, 0.19);" class="padding-tb">
+				<div class="grid-3">
+					<div class="container-80">
+						<div class="dropdown">
+							<a>Programming Languages</a>
+							<div class="dropdown-content">
+							    <a href="#">C & C++ Programming</a>
+							    <a href="#">Python Programming</a>
+							    <a href="#">Data Science</a>
+							    <a href="#">Machine Learning</a>
+							    <a href="#">Java Programming</a>
+							    <a href="#">PHP Programming</a>
+							    <a href="#">MYSQL Programming</a>
+							    <a href="#">PHP & MYSQL Programming</a>
+							</div>
+						</div>
+					</div>
+					<div class="container-80">
+						<div class="dropdown">
+							<a>Web Technologies</a>
+							<div class="dropdown-content">
+							    <a href="#">Full Stack Web Development</a>
+							    <a href="#">Front-End Development</a>
+							    <a href="#">UI Development</a>
+							    <a href="#">Back-End Development</a>
+							    <a href="#">SEO Development</a>
+							    <a href="#">Digital Marketing Course</a>
+							    <a href="#">Python & Flask</a>
+							</div>
+						</div>
+					</div>
+					<div class="container-80">
+						<div class="dropdown">
+							<a>Web Programming</a>
+							<div class="dropdown-content">
+							    <a href="#">HTML4 & HTML5</a>
+							    <a href="#">CSS & CSS3</a>
+							    <a href="#">Bootstrap</a>
+							    <a href="#">JavaScript course</a>
+							    <a href="#">JQuery</a>
+							    <a href="#">ReactJS</a>
+							    <a href="#">AngularJS</a>
+							    <a href="#">SaSS</a>
+							    <a href="#">NextJS</a>
+							    <a href="#">JSON</a>
+							    <a href="#">API</a>
+							    <a href="#">AJAX</a>
+							    <a href="#">Digital Marketing </a>
+							    <a href="#">Java Programming</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<section>
+			<div>
+				<div class="margin-tb">
+					<img src="image/bg.jpg" class="img">
+				</div>
+			</div>
+		</section>
+		<article>
+			<div class="container-80">
+				<div>
+					<p class="text-center fs-20">Java Programming | Python Programming | C&&C++ Programming | PHP Programming | Digital Marketing | App Development | Web <br> Development | Web Designing | PHP and MySqL Professional Courses in Bilaspur Chhattisgarh</p>
+				</div>
+			</div>
+		</article>
+		<section class="course-list">
+			<div class="container-80">
+				<div class="margin-tb">
+					<p class="fs-20"><strong>Baba Programmer</strong>  provides Job oriented training on this Technologies Listed below:</p>
+					<ol>
+						<li>
+							<p>Java Programming</p>
+						</li>
+						<li>
+							<p>Python Programming</p>
+						</li>
+						<li>
+							<p>C C++ Programming</p>
+						</li>
+						<li>
+							<p>Php & Mysql</p>
+						</li>
+						<li>
+							<p>Digital Marketing</p>
+						</li>
+						<li>
+							<p>SEO (Search Engine Optimization) Development</p>
+						</li>
+						<li>
+							<p>Web Designing</p>
+						</li>
+						<li>
+							<p>Website Development</p>
+						</li>
+						<li>
+							<p>UI Development</p>
+						</li>
+						<li>
+							<p>Front end Development</p>
+						</li>
+						<li>
+							<p>Backend Development</p>
+						</li>
+						<li>
+							<p>App Development</p>
+						</li>
+						<li>
+							<p>Android App Development</p>
+						</li>
+						<li>
+							<p>Ios App Development</p>
+						</li>
+						<li>
+							<p>HTML5 && CSS3</p>
+						</li>
+						<li>
+							<p>SASS (Syntactically Awesome Style Sheets)</p>
+						</li>
+						<li>
+							<p>JavaScript(ES5)|JavaScript(ES6)</p>
+						</li>
+						<li>
+							<p>Bootstrap3 && Bootstrap4</p>
+						</li>
+						<li>
+							<p>WordPress</p>
+						</li>
+						<li>
+							<p>Jquery</p>
+						</li>
+						<li>
+							<p>AngularJs</p>
+						</li>
+						<li>
+							<p>ReactJs</p>
+						</li>
+						<li>
+							<p>Json</p>
+						</li>
+						<li>
+							<p>Ajax</p>
+						</li>
+						<li>
+							<p>D3Js</p>
+						</li>
+						<li>
+							<p>API ( Application Programming Interface )</p>
+						</li>
+						<li>
+							<p>ReactNative</p>
+						</li>
+						<li>
+							<p>Full Stack Development</p>
+						</li>
+						<li>
+							<p>Data Structure</p>
+						</li>
+						<li>
+							<p>SMM ( Social Media Marketing )</p>
+						</li>
+						<li>
+							<p>Email Marketing</p>
+						</li>
+						<li>
+							<p>Local SEO</p>
+						</li>
+						<li>
+							<p>On Page SEO and Off Page SEO</p>
+						</li>
+						<li>
+							<p>Web Application</p>
+						</li>
+					</ol>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container-90">
+				<div class="grid-4 proCourse">
+					
+				</div>
+			</div>
+		</section>
+
+		<section></section>
+
+		<footer>
+			<div>
+				<div class="grid-4">
+					<div>
+						<p>COURSES</p>
+						<ul id="courses">
+							
+						</ul>
+					</div>
+					<div>
+						<p>INTERVIEW QUESTIONS</p>
+						<ul id="qiestions">
+							
+						</ul>
+					</div>
+					<div>
+						<p>CORPORATE SERVICES</p>
+						<ul id="corporate">
+							
+						</ul>
+					</div>
+					<div>
+						<p>OFFICE ADDRESS</p>
+						<ul id="office">
+							
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</main>
+
+	<script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,149 @@
+// Courses List
+var proCourse = document.querySelector(".proCourse");
+let arr = [
+  {
+    title: "Php and Mysql Course",
+    img: "image/1.jpg",
+    desc: "Learn to use PHP professionally, learning both Core PHP and Advance PHP.",
+  },
+  {
+    title: "Python Course",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS94gXDs1ZUOskVCs_hd9zgEd3yJKKI5dWn-g&usqp=CAU",
+    desc: "Learn to use Python professionally, learning both Python 2 and Python 3!",
+  },
+  {
+    title: "Java Course",
+    img: "https://online.crbtech.in/wp-content/uploads/2019/03/Core-Java-Pro.jpg",
+    desc: "Learn Java In This Course And Become a Computer Programmer.",
+  },
+  {
+    title: "C and C++ Course",
+    img: "https://www.itrainu.in/wp-content/uploads/2022/01/c-c-img.jpg",
+    desc: "Learn C and C++ In This Course And Become a Computer Programmer.",
+  },
+  {
+    title: "HTML and HTML5 Course",
+    img: "https://res.cloudinary.com/go1/image/upload/q_60,h_256/v1558665969/acxk7reamabpmadi3tep.png",
+    desc: "Learn to use Html professionally, learning both HTML and HTML5.",
+  },
+  {
+    title: "Css and Css3",
+    img: "https://colorlib.com/wp/wp-content/uploads/sites/2/creative-css3-tutorials.jpg",
+    desc: "Learn to use Css professionally, learning both Css and Css 3!",
+  },
+  {
+    title: "Bootstrap",
+    img: "https://www.etatvasoft.com/insights/wp-content/uploads/2017/02/b-thumb-img9.jpg",
+    desc: "Learn to use Bootstrap professionally, learning both Bootstrap3 and Bootstrap4",
+  },
+  {
+    title: "JavaScript",
+    img: "https://miro.medium.com/v2/resize:fit:1200/1*LyZcwuLWv2FArOumCxobpA.png",
+    desc: "Learn to use Javascript professionally, learning both Javascript and Javascript6",
+  },
+  {
+    title: "Jquery Course",
+    img: "https://www.zdnet.com/a/img/resize/1353418965b69841eba1da18244e9e012b8afea5/2019/04/21/9d791bf2-9b20-476a-bba6-201e622d2a1f/jquery-logo-blue.png?auto=webp&fit=crop&height=675&width=1200",
+    desc: "Learn to use Jquery professionally, learning both Jquery3.0.0 and Jquery3.3.1.",
+  },
+  {
+    title: "AngularJs",
+    img: "https://miro.medium.com/v2/resize:fit:1400/1*R1mfXLP9edcArZXwmGbGag.jpeg",
+    desc: "Learn to use AngularJs professionally, learning AngularJs.Learn to use ReactJs professionally, learning ReactJs.",
+  },
+  {
+    title: "ReactJs",
+    img: "https://www.unicorn.dev/wp-content/uploads/2023/01/react.png",
+    desc: "Learn to use ReactJs professionally, learning ReactJs.",
+  },
+  {
+    title: "Json",
+    img: "https://www.crio.do/blog/content/images/2021/06/What-is-JSON.png",
+    desc: "Learn to use Json professionally, learning Json .",
+  },
+  {
+    title: "Digital Marketing",
+    img: "https://www.icscareergps.com/blog/wp-content/uploads/2023/11/digital-marketing-benefits.jpg",
+    desc: "Learn to use Digital Marketing professionally, learning SEO,SMM,PPC,Email Marketing",
+  },
+  {
+    title: "Django",
+    img: "https://www.yarddiant.com/images/best-front-end-framework-for-django.jpg",
+    desc: "Learn to use Django(Python framework) professionally.",
+  },
+  {
+    title: "Laravel framework",
+    img: "https://picperf.io/https://laravelnews.s3.amazonaws.com/images/laravel-featured.png",
+    desc: "Learn to use Laravel Framework professionally.Laravel is a powerful MVC PHP framework.",
+  },
+  {
+    title: "SEO",
+    img: "https://www.oberlo.com/media/1603954182-seo-article-header.png",
+    desc: "Learn to use SEO professionally, Start your SEO Business.",
+  },
+];
+
+for (var i in arr) {
+  //   console.log(arr[i]);
+  proCourse.innerHTML += `
+			<div class="margin-tb">
+				<div  class="container-90">
+						<div class="">
+							<p class="text-center bg-white padding-tb">${arr[i].title}</p>
+						</div>
+						<div>
+							<img src="${arr[i].img}" class="img">
+						</div>
+						<div class="padding-tb bg-white border-bottom">
+							<p>${arr[i].desc}</p>
+							<p class="text-blue">See Course Content</p>
+						</div>
+					</div>
+			</div>
+			
+		`;
+}
+
+// Footer
+
+// console.log(courseArr);
+
+function footer(coursList) {
+  for (var i in coursList) {
+    // console.log(courseArr[i]);
+
+    courses.innerHTML += `
+		  <li>
+	  		<a>${coursList[i]}</a>
+		  </li>
+		`;
+  }
+}
+
+function footerArray() {
+  let courseArr = [
+    "HTML AND HTML5",
+    "CSS AND CSS3",
+    "BOOTSTRAP4",
+    "JAVASCRIPT",
+    "JQUERY",
+    "AJAX",
+    "ANGULARJS",
+    "REACTJS",
+    "JSON",
+    "C AND C++",
+    "JAVA",
+    "PHP AND MYSQL",
+    "PYTHON",
+    "SEO",
+    "DIGITAL MARKETING",
+    "DJANGO",
+    "LARAVEL",
+  ];
+
+  let QuestionsArray = [];
+
+  footer(courseArr);
+}
+
+footerArray();
